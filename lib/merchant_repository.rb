f6a4382f4eb24a282765c6732907cd4f42715728
MaxMillington <maxmillington89@gmail.com>
2015-06-15 17:41:04 -0600

    added text stating what we are looking for

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,9 @@
+
+#For each repository:
+# 1) "all" returns all instances
+# 2) "random" returns a random instances
+# 3) "find_by_X(match)", where X is some attribute, returns a single instance whose X attribute case-insensitive
+# attribute matches the (match) parameter. E.G. 'customer_repository.find_by_first_name("Mary")' could find
+# a customer with the first name "Mary" or "mary" but not "Mary Ellen".
+# 4)"find_by_all_X(match)", works just like "find_by_X(match)" except it returns all instances of the match
+# as opposed to merely one instance. If there are no matches it returns an empty array.
